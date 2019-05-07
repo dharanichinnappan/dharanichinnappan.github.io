78be0b8481c2404367803ed8d80c22a05b3829db
dharanichinnappan <43676141+dharanichinnappan@users.noreply.github.com>
2019-05-07 13:54:00 +0530

    updating controller.js

--- SMS_Frontend_Task/js/controller.js
+++ SMS_Frontend_Task/js/controller.js
@@ -4,33 +4,87 @@ myApp.controller('indexController', function($scope, $http, $filter) {
 	$scope.from = "";
 	$scope.to = "";
 	$scope.load = function() {
+		console.log("load function is being called");
 		$http.get('data.json').success(function(data) {
 			$scope.datas = data;
-			$scope.copy_of_datas = $scope.datas;
+			$scope.copy_of_datas = data;
 		})
 	}
 
 	$scope.sort_asc = function(data) {
-		if (data == 'date') {
-			
+		if (data == 'start_date') {
+
+			$scope.copy = $scope.copy_of_datas;
+			angular.forEach($scope.copy, function(value, key) {
+
+				$scope.start_date = new Date(value.start_date);
+				value.start_date = $scope.start_date;
+			})
+
+			$scope.datas = $filter('orderBy')($scope.copy, data, false);
+			angular.forEach($scope.datas, function(value, key) {
+
+				$scope.start_date = value.start_date.toLocaleDateString();
+				value.start_date = $scope.start_date;
+			})
+
+		}
+		if (data == 'end_date') {
+			$scope.copy = $scope.copy_of_datas;
+			angular.forEach($scope.copy, function(value, key) {
+				$scope.end_date = new Date(value.end_date);
+				value.end_date = $scope.end_date;
+			})
+
+			$scope.datas = $filter('orderBy')($scope.copy, data, false);
+			angular.forEach($scope.datas, function(value, key) {
+				$scope.end_date = value.end_date.toLocaleDateString();
+				value.end_date = $scope.end_date;
+				console.log(value.end_date);
+			})
 		} else {
 			$scope.datas = $filter('orderBy')($scope.datas, data);
 		}
 	}
 
 	$scope.sort_des = function(data) {
-		if (data == 'date') {
+		if (data == 'start_date') {
+
+			$scope.copy = $scope.copy_of_datas;
+			angular.forEach($scope.copy, function(value, key) {
+
+				$scope.start_date = new Date(value.start_date);
+				value.start_date = $scope.start_date;
+			})
+
+			$scope.datas = $filter('orderBy')($scope.copy, data, true);
+			angular.forEach($scope.datas, function(value, key) {
+
+				$scope.start_date = value.start_date.toLocaleDateString();
+				value.start_date = $scope.start_date;
+
+			})
 		} else {
 			$scope.datas = $filter('orderBy')($scope.datas, data, true);
 		}
 	}
 
 	$scope.filter = function(from, to) {
-
+		$scope.n = 0;
+		angular.forEach($scope.datas, function(value, key) {
+			if (value != null) {
+				$scope.n++;
+			}
+		})
+		console.log($scope.n + " is $scope.datas at the begging");
+		
 		$scope.from = from;
 		$scope.to = to;
-
-		angular.forEach($scope.copy_of_datas, function(value, key) {
+		$scope.load();
+		var count = Object.keys($scope.datas).length;
+		console.log(count + " is $scope.datas before looping after loading size");
+		
+		angular.forEach($scope.datas, function(value, key) {
 			if (value != null) {
 				$scope.start_date = new Date(value.start_date);
 				$scope.end_date = new Date(value.end_date);
@@ -48,7 +102,14 @@ myApp.controller('indexController', function($scope, $http, $filter) {
 			}
 
 		});
-
+		$scope.n=0;
+		angular.forEach($scope.datas, function(value, key) {
+			if (value != null) {
+				$scope.n++;
+			}
+		})
+		
+		console.log($scope.n + " is $scope.datas after filtering");
 	}
 
 	$scope.reset = function() {
