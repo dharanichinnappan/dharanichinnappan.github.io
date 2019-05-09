e0f52bd56c671c3251002ea4bcba6731e9994521
dharanichinnappan <43676141+dharanichinnappan@users.noreply.github.com>
2019-05-09 17:47:31 +0530

    updated controller.js

--- SMS_Frontend_Task/js/controller.js
+++ SMS_Frontend_Task/js/controller.js
@@ -8,45 +8,46 @@ myApp
 					$scope.login = function() {
 						$scope.pattern = "^[^@]+@[^@]+\.[^@]+$";
 						var regex = new RegExp($scope.pattern);
+						if ($scope.email == null || $scope.password == null
+								|| $scope.confirm_password == null) {
+							if ($scope.email == null) {
+								document.getElementById("email")
+										.setCustomValidity(
+												"Please enter a email");
+							} else if ($scope.password == null) {
+								document
+										.getElementById("password")
+										.setCustomValidity(
+												"Please enter a password.Password must contain atleast 8 characters");
+							} else if ($scope.confirm_password == null
+									|| $scope.confirm_password != $scope.password) {
+								document.getElementById("confirm_password")
+										.setCustomValidity(
+												"Password does not match");
+								document.getElementById("confirm_password").style.outlineColor = "red";
+							}
+						}
 
-						if ($scope.email == null) {
-							document.getElementById("email").setCustomValidity(
-									"Please enter a email");
+						else {
+							if ($scope.email != null
+									&& regex.test($scope.email) == false) {
+								document.getElementById("email")
+										.setCustomValidity(
+												"Enter a valid email ");
+							} else if ($scope.password != null
+									&& $scope.password.length < 8) {
+								document
+										.getElementById("password")
+										.setCustomValidity(
+												"Password must contain atleast 8 characters");
+							} else if ($scope.password != null
+									&& $scope.confirm_password != null
+									&& $scope.password.length >= 8
+									&& $scope.password == $scope.confirm_password) {
+								location.href = "index.html";
+							}
 						}
-						/*
-						 * Checking email is not null and whether email matches
-						 * regex pattern
-						 */
-						else if ($scope.email != null
-								&& regex.test($scope.email) == false) {
-							document.getElementById("email").setCustomValidity(
-									"Enter a valid email ");
-						}
-						if ($scope.password == null) {
-							document
-									.getElementById("password")
-									.setCustomValidity(
-											"Please enter a password.Password must contain atleast 8 characters");
-						} else if ($scope.password != null
-								&& $scope.password < 8) {
-							document
-									.getElementById("password")
-									.setCustomValidity(
-											"Password must contain atleast 8 characters");
-						}
-						if ($scope.confirm_password == null
-								|| $scope.confirm_password != $scope.password) {
-							document.getElementById("confirm_password")
-									.setCustomValidity(
-											"Password does not match");
-							document.getElementById("confirm_password").style.outlineColor = "red";
-						}
-						if ($scope.password != null
-								&& $scope.confirm_password != null
-								&& $scope.password.length >= 8
-								&& $scope.password == $scope.confirm_password) {
-							location.href = "index.html";
-						}
+
 						document.getElementById("email").oninput = function() {
 							document.getElementById("email").setCustomValidity(
 									'');
@@ -62,7 +63,6 @@ myApp
 						}
 
 					}
-					
 					$scope.showPassword = function(event) {
 						if (event.target.checked == true) {
 							$scope.password_type = 'text';
